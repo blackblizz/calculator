3ceeee36b86a403a20a924ef3370dee1ea35d4b5
Cassidy Ng <gccn212@gmail.com>
2022-11-23 23:31:20 -0600

    as of nov23

---
+++ cal.js
@@ -0,0 +1,7 @@
+const numberBtn = document.querySelectorAll('[data-number');
+const operationBtn = document.querySelectorAll('[data-op');
+const allClearBtn = document.querySelector('[data-ac]');
+const deleteBtn = document.querySelector('data-del');
+const equalBtn = document.querySelector('[data-equal');
+const upperTxt= document.querySelector('[data-upper');
+const lowerTxt= document.querySelector('[data-lower');
